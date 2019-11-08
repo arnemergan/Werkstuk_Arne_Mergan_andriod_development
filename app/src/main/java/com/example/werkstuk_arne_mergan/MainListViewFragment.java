72d9959c6ba1d57e6ad6cc96f3d5679683283e5d
arnemergan <mergan.arne@hotmail.com>
2019-11-08 17:25:16 +0100

    second commit

---
+++ app/src/main/java/com/example/werkstuk_arne_mergan/MainListViewFragment.java
@@ -0,0 +1,31 @@
+package com.example.werkstuk_arne_mergan;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainListViewFragment extends Fragment {
+
+
+    public MainListViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_list_view, container, false);
+    }
+
+}
